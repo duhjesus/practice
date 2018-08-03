aedcf45e2f8cca97b37142cf320ba31315f36dcd
duhjesus <leon.jesusleon@gmail.com>
2018-08-03 00:57:34 -0700

    basic graph

--- hackerrank/graph.py
+++ hackerrank/graph.py
@@ -6,22 +6,33 @@ class Node():
         self.next= None
 
 class LinkedList():
-    def __init__(self):
-        self.head = None
-        self.tail = None
-    def insert(self,id):
+    def __init__(self,head):
+        self.head = head
+        self.tail = head
+    def add(self,nodeToAdd):
         if self.head == None:
-            self.head =Node(id)
+            self.head =nodeToAdd
             self.head = self.tail
         elif self.head == self.tail:
-            self.tail= Node(id)
+            self.tail= nodeToAdd
             self.head.next = self.tail
         else:
-            self.tail.next = Node(id)
+            self.tail.next = nodeToAdd
             self.tail = self.tail.next
+    # def insert(self,id):
+    #     if self.head == None:
+    #         self.head =Node(id)
+    #         self.head = self.tail
+    #     elif self.head == self.tail:
+    #         self.tail= Node(id)
+    #         self.head.next = self.tail
+    #     else:
+    #         self.tail.next = Node(id)
+    #         self.tail = self.tail.next
     def delete(self,val):
         if self. head == None:
-            return "Empty: No deletion"
+            print("Empty: No deletion")
+            return
         else:
             curr = self.head
             before = self.head
@@ -34,7 +45,7 @@ class LinkedList():
                         before.next= curr.next
 
                     curr.next =None
-                    print("Deleted Node containing=",val)
+                    print("Deleted Node id=",val)
                     break
                 else:
                     if curr ==self.head:
@@ -43,17 +54,90 @@ class LinkedList():
                     else:
                         before =curr
                         curr = curr.next
-            
+
+#!
+#ctrl +/ results in entire code block being edited out
+#!
+
+class Vertex():
+    def __init__(self,index):
+        self.id = index
+        self.next= None
+        self.visited = False
 class Graph():
     def __init__(self):
-        # mapping from nodeid to mem
-        self.adjMatrix = []
-        self.adjList = {}
-        
-    def addVertex(self,)
-def main():
-    pass
+        self.adjList = []#aadjacence list rray of linkedlists where head is
+                         #vertex and all nodes in list are neighbors
+        self.nodelookup = {} # easily access node's idx in the adjlist
+                             # key = node's id, val = idx in adjlist
+    #create's the graph structure by
+    #finding actual nodes of graph in the grid
+    #and by generating an adjlist
+    #
+    def create(self, grid, n , m):
+        for i in range(0,n):
+            for j in range(0,m):
+                if grid[i][j] == 0:
+                    continue
+                index = i*m+j
+                curr = Vertex(index)
+                templl = LinkedList(curr)
+                self.nodelookup[index] = len(self.adjList)
+                x = []
+                x.append([i-1,j-1])
+                x.append([i-1,j])
+                x.append([i-1,j+1])
+                x.append([i,j-1])
+                x.append([i,j+1])
+                x.append([i+1,j-1])
+                x.append([i+1,j])
+                x.append([i+1,j+1])
+                for position in x:
+                    if position[0] <0 or position[0]>=n:
+                        continue                        
+                    elif position[1] <0 or position[1]>=m:
+                        continue 
+                    elif grid[position[0]][position[1]] == 0:
+                        continue 
+                    else:
+                        idx = position[0]*m+position[1]
+                        neighbor = Vertex(idx)
+                        templl.add(neighbor)
+                self.adjList.append(templl)
 
+    def dfsCount(self):
+        if len(self.adjList) == 0:
+            return 0
+        maxx = 0
+        adjIndex = 0
+        while adjIndex < len(self.adjList): # goes thru all the disconnected regions
+            cnt = 0
+            stack = []
+            curr = self.adjList[adjIndex].head            
+            if curr.visited == False: 
+                stack.append(curr)                
+                while len(stack) != 0: # goes thru all the connected cells w/in one region
+                    v = stack.pop()                    
+                    if v.visited == False:
+                        v.visited = True
+                        cnt = cnt + 1 # keeps track of current region's 
+                                      # current amt of found vertices
+                        vhead = v.next                      
+                        while( vhead != None):
+                            nodeIndex =self.nodelookup[vhead.id] # lookup node's idx in the list
+                            stack.append(self.adjList[nodeIndex].head)
+                            vhead = vhead.next                        
+
+            adjIndex = adjIndex + 1
+            if maxx < cnt:
+                maxx = cnt
+        return maxx
+
+def main():
+    g = Graph()
+    g.create(grid, n, m)
+    cnt = g.dfsCount()
+    print(cnt)
 
 if __name__=="__main__":
     main()
